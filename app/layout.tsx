0c385490b653dd3cbe5828a77ee6ceca9f91ce13
mikkelvs <mikkelvs@gmail.com>
2026-05-02 12:55:07 +0200

    remove character

--- app/layout.tsx
+++ app/layout.tsx
@@ -64,7 +64,7 @@ export default async function RootLayout({
         <div className="max-w-7xl mx-auto px-4 md:px-8">
           <TopNavigation />
           <Header />
-          {children}s
+          {children}
           <Analytics />
           <GoogleAnalytics gaId={process.env.GA_TRACKING_ID || ""} />
           <footer>
